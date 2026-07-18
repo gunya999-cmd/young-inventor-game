fd3a63812f2cf582c0f2d929273050ec6505d8ad
gunya999-cmd <gunya999@gmail.com>
2026-07-18 11:46:03 +0300

    Export clean-room TIM core

---
+++ src/tim-core/index.ts
@@ -0,0 +1,17 @@
+export { FixedStepClock } from './FixedStepClock';
+export { MachineModel } from './MachineModel';
+export type {
+  AnchorDefinition,
+  AnchorKind,
+  Connection,
+  ConnectionId,
+  Endpoint,
+  MachineMode,
+  MachineSnapshot,
+  PartDefinition,
+  PartId,
+  PartInstance,
+  PartKind,
+  Transform,
+  Vec2
+} from './types';
