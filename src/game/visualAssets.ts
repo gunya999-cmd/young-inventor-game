0810caf23a108ff92d56a49568c47ed66bd27da8
gunya999-cmd <gunya999@gmail.com>
2026-07-18 10:37:55 +0300

    Use optimized V3 visual asset set

--- src/game/visualAssets.ts
+++ src/game/visualAssets.ts
@@ -1,7 +1,14 @@
 // @ts-nocheck
 import { ASSET_BACKGROUND_IMAGE } from './assetBackgroundImage';
 import { ASSET_STATIC_SPRITES } from './assetStaticSprites';
-import { ASSETS_MECHANISM_A } from './assetsMechanismA';
-import { ASSETS_MECHANISM_B } from './assetsMechanismB';
+import { ASSET_BALL } from './asset_ball';
+import { PROCEDURAL_MECHANISM_ASSETS } from './proceduralMechanismAssets';
 import { ASSETS_PARTS } from './assetsParts';
-export const VISUAL_ASSETS = { ...ASSET_BACKGROUND_IMAGE, ...ASSET_STATIC_SPRITES, ...ASSETS_MECHANISM_A, ...ASSETS_MECHANISM_B, ...ASSETS_PARTS } as const;
+
+export const VISUAL_ASSETS = {
+  ...ASSET_BACKGROUND_IMAGE,
+  ...ASSET_STATIC_SPRITES,
+  ...ASSET_BALL,
+  ...PROCEDURAL_MECHANISM_ASSETS,
+  ...ASSETS_PARTS
+} as const;
