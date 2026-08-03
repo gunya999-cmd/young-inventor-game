58a2b7f37973752cba4de7f907cd831b613bab67
gunya999-cmd <gunya999@gmail.com>
2026-08-03 12:32:26 +0300

    Route Part 15 electric motor asset lab

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ import { installCannonballLab } from './cannonballLab';
 import { installPart0408Lab, isPart0408Asset } from './parts0408Lab';
 import { installPart0913Lab, isPart0913Asset } from './parts0913Lab';
 import { installPart14PulleyLab } from './part14Lab';
+import { installPart15MotorLab } from './part15Lab';
 import { installCompletionGuard } from './gameFlowGuard';
 import { installEditorUiIntegration } from './editorUiIntegration';
 import { installBrowserSmokeBridge } from './e2eBridge';
@@ -51,6 +52,8 @@ if (assetPreview === 'bowling-ball') {
   installCannonballLab();
 } else if (assetPreview === 'pulley') {
   installPart14PulleyLab();
+} else if (assetPreview === 'motor') {
+  installPart15MotorLab();
 } else if (isPart0408Asset(assetPreview)) {
   installPart0408Lab(assetPreview);
 } else if (isPart0913Asset(assetPreview)) {
