961f2d472a88db59eef6ecf70dadb6e233642983
gunya999-cmd <gunya999@gmail.com>
2026-08-04 21:03:33 +0300

    Route scissors preview to v12 physical cut fix

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ import { installRopeLab } from './ropeLab';
 import { installMetalLoopHookLab } from './metalLoopHookLab';
 import { installRevolverLab } from './revolverLab';
 import { installScissors2DLab } from './scissors2dLab';
-import { installScissors3DLabV10 } from './scissors3dLabV10';
+import { installScissors3DLabV12 } from './scissors3dLabV12';
 import { installOutletSwitchLab } from './outletSwitchLab';
 import { installCompletionGuard } from './gameFlowGuard';
 import { installEditorUiIntegration } from './editorUiIntegration';
@@ -75,7 +75,7 @@ if (assetPreview === 'bowling-ball') {
 } else if (assetPreview === 'scissors-2d') {
   installScissors2DLab();
 } else if (assetPreview === 'scissors' || assetPreview === 'scissors-3d') {
-  installScissors3DLabV10();
+  installScissors3DLabV12();
 } else if (assetPreview === 'outlet-switch' || assetPreview === 'switch-outlet') {
   installOutletSwitchLab();
 } else if (isPart0408Asset(assetPreview)) {
