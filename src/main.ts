c95321ddb80d6bdcd6536ded0c046b289d8d8f39
gunya999-cmd <gunya999@gmail.com>
2026-08-04 19:23:44 +0300

    Expose Classic Part 21 outlet switch preview

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ import { installMetalLoopHookLab } from './metalLoopHookLab';
 import { installRevolverLab } from './revolverLab';
 import { installScissors2DLab } from './scissors2dLab';
 import { installScissors3DLabV9 } from './scissors3dLabV9';
+import { installOutletSwitchLab } from './outletSwitchLab';
 import { installCompletionGuard } from './gameFlowGuard';
 import { installEditorUiIntegration } from './editorUiIntegration';
 import { installBrowserSmokeBridge } from './e2eBridge';
@@ -75,6 +76,8 @@ if (assetPreview === 'bowling-ball') {
   installScissors2DLab();
 } else if (assetPreview === 'scissors' || assetPreview === 'scissors-3d') {
   installScissors3DLabV9();
+} else if (assetPreview === 'outlet-switch' || assetPreview === 'switch-outlet') {
+  installOutletSwitchLab();
 } else if (isPart0408Asset(assetPreview)) {
   installPart0408Lab(assetPreview);
 } else if (isPart0913Asset(assetPreview)) {
