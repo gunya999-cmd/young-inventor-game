ed06e0108c1beffa4c9e7c0cbd847cb4be56d39a
gunya999-cmd <gunya999@gmail.com>
2026-08-07 08:40:08 +0300

    Route clean minimal Level 01 preview

--- src/main.ts
+++ src/main.ts
@@ -39,18 +39,27 @@ function showFatalError(error: unknown): void {
 window.addEventListener('error', (event) => showFatalError(event.error ?? event.message));
 window.addEventListener('unhandledrejection', (event) => showFatalError(event.reason));
 
-const assetPreview = new URLSearchParams(location.search).get('asset');
+const params = new URLSearchParams(location.search);
+const assetPreview = params.get('asset');
+const levelPreview = params.get('level');
 
-if (assetPreview === 'bowling-ball') {
-  installBowlingBallLab();
-} else if (assetPreview === 'basketball') {
-  installBasketballLab();
-} else if (assetPreview === 'cannonball') {
-  installCannonballLab();
-} else if (isPart0408Asset(assetPreview)) {
-  installPart0408Lab(assetPreview);
-} else {
-  try {
+async function boot(): Promise<void> {
+  if (levelPreview === 'clean01') {
+    await import('./cleanMinimalLevel01.css');
+    const { installCleanMinimalLevel01 } = await import('./cleanMinimalLevel01');
+    installCleanMinimalLevel01();
+    return;
+  }
+
+  if (assetPreview === 'bowling-ball') {
+    installBowlingBallLab();
+  } else if (assetPreview === 'basketball') {
+    installBasketballLab();
+  } else if (assetPreview === 'cannonball') {
+    installCannonballLab();
+  } else if (isPart0408Asset(assetPreview)) {
+    installPart0408Lab(assetPreview);
+  } else {
     installExtendedParts();
     installPulleySystem();
     installSpringSystem();
@@ -71,7 +80,7 @@ if (assetPreview === 'bowling-ball') {
     installLevelEditor(app);
     installCompletionGuard();
     installBrowserSmokeBridge(app);
-  } catch (error) {
-    showFatalError(error);
   }
 }
+
+boot().catch(showFatalError);
